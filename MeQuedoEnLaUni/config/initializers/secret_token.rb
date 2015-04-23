1b1e8e48dc74e3e91371791ec869ca4b6c54bba1
Alberto MOta <mota_al@hotmail.com>
2015-04-22 23:56:33 -0500

    primer push

---
+++ MeQuedoEnLaUni/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeQuedoEnLaUni::Application.config.secret_key_base = '0b2b71a99285c73fa84a2c823dbee8bd8ae097d52c2fe0db36aed214f11b95229242fabda4834e869ef773c1476c770ba41f75b42ee66506a0b51317c79ad0d1'
